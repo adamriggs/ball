c8680165db0f80cb6588ecf988e2842daa89e8d0
Adam Riggs <riggs@icscreative.com>
2026-03-15 06:23:42 -0400

    movement

--- src/js/ball.js
+++ src/js/ball.js
@@ -6,6 +6,13 @@ const contentElem = document.getElementById('content');
 const backgroundColor = 0xABDADC;
 const ballColor = 0xFF4400;
 
+let floor = window.innerHeight - 50;
+let cieling = 50;
+let speed = 10;
+
+let prevTime = 0;
+let reverse = false;
+
 const initApp = async() => {
 	await app.init({
 		width: '100vw',
@@ -26,7 +33,34 @@ const initBall = () => {
 	app.stage.addChild(ball);
 }
 
+const animate = (timestamp) => {
+
+	if (!prevTime) {
+		prevTime = timestamp;
+	}
+	const deltaTime = timestamp - prevTime;
+	prevTime = timestamp;
+
+	if (reverse) {
+		ball.y -= speed;
+	} else {
+		ball.y += speed;
+	}
+
+	if (ball.y >= floor) {
+		reverse = true;
+	}
+
+	if (ball.y <= cieling) {
+		reverse = false;
+	}
+
+	requestAnimationFrame(animate);
+}
+
 window.addEventListener('load', () => {
 	initApp();
 	initBall();
+
+	requestAnimationFrame(animate);
 });
